64cdd223468157e91f9a86a228a7fdc53165472d
muhammedalmanie <94794248+muhammedalmanie@users.noreply.github.com>
2023-08-31 11:29:27 +0300

    testtrf

---
+++ gfcx/gfcx/gfcxApp.swift
@@ -0,0 +1,17 @@
+//
+//  gfcxApp.swift
+//  gfcx
+//
+//  Created by Muhammed on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct gfcxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
